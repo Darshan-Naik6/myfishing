1909d3db053aff2fa9ae868b6aa0698a9e33d7ab
Darshan-Naik6 <naikdarshan150@gmail.com>
2024-02-24 01:51:05 +0530

    first commit

--- android/app/src/main/kotlin/com/example/myfishing/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myfishing
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,221 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Fishing',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+ // ignore: library_private_types_in_public_api
+ _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  static List<Widget> _widgetOptions = <Widget>[
+    Section1Page(),
+    Section2Page(),
+    Section3Page(),
+    Section4Page(),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background_image.jpg'), // Path to your background image
+            fit: BoxFit.cover,
+          ),
+        ),
+      child: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(
+        title: Padding( // Added Padding widget
+          padding: const EdgeInsets.symmetric(vertical: 10.0), // Adjusted top padding
+          child: Center(
+            child: Text(
+              'My Fishing',
+              style: TextStyle(
+                fontSize: 42,
+                   color: Colors.blue
+              ),
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map_rounded),
+            label: 'Climate Monsoon Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map_outlined),
+            label: 'Fishing Ships Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time_sharp),
+            label: 'Coastal Zones',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.maps_ugc_sharp),
+            label: 'Education Resources',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
+
+class Section1Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding( // Added Padding widget
+          padding: const EdgeInsets.only(top: 20.0), // Adjusted top padding
+          child: Center(child: Text('Climate Monsoon Map', style: TextStyle(color: const Color.fromARGB(255, 193, 52, 218)))),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CardWidget(title: 'Monsoon Tracker', color: Color.fromARGB(255, 241, 128, 128)),
+            CardWidget(title: 'Disaster Alert', color: Color.fromARGB(255, 241, 128, 128)),
+            CardWidget(title: 'Ban Alert', color: Color.fromARGB(255, 241, 128, 128)),
+            CardWidget(title: 'Climate Tracker', color: Color.fromARGB(255, 241, 128, 128)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Section2Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding( // Added Padding widget
+          padding: const EdgeInsets.only(top: 20.0), // Adjusted top padding
+          child: Center(child: Text('Fishing Ships Map', style: TextStyle(color: const Color.fromARGB(255, 193, 52, 218)))),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CardWidget(title: 'Fish Levels', color: Colors.blue),
+            CardWidget(title: 'Concentration of Ships', color: Colors.blue),
+            CardWidget(title: 'Patterns', color: Colors.blue),
+            CardWidget(title: 'Violations', color: Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Section3Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding( // Added Padding widget
+          padding: const EdgeInsets.only(top: 20.0), // Adjusted top padding
+          child: Center(child: Text('Coastal Zones', style: TextStyle(color: const Color.fromARGB(255, 193, 52, 218)))),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CardWidget(title: 'Land', color: Color.fromARGB(255, 221, 240, 80)),
+            CardWidget(title: 'Water', color: Color.fromARGB(255, 221, 240, 80)),
+            CardWidget(title: 'Protected Ecosystem', color: Color.fromARGB(255, 221, 240, 80)),
+            CardWidget(title: 'Green Zone', color: Color.fromARGB(255, 221, 240, 80)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Section4Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding( // Added Padding widget
+          padding: const EdgeInsets.only(top: 20.0), // Adjusted top padding
+          child: Center(child: Text('Education Resources', style: TextStyle(color: Color.fromARGB(255, 207, 21, 240)))),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CardWidget(title: 'Websites', color: Color.fromARGB(255, 147, 237, 91)),
+            CardWidget(title: 'Helplines', color: Color.fromARGB(255, 147, 237, 91)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  const CardWidget({super.key, required this.title, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(25),
+      color: color,
+      child: InkWell(
+        onTap: () {
+          // Handle card tap
+        },
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
